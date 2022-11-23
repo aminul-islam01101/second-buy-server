9fa2643e31dbe5049ea1aaebd9b2a4974583a174
aminulislam01101 <aminul.islam01101@gmail.com>
2022-11-24 00:04:19 +0600

    First commit-Folder setup

---
+++ index.js
@@ -0,0 +1,45 @@
+import colors from 'colors';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+
+// port and env
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+colors.setTheme({
+    info: 'green',
+    help: 'cyan',
+    warn: 'yellow',
+    error: 'red',
+});
+
+app.get('/', (_req, res) => {
+    res.send('First test server ');
+});
+// Error middleware
+// 404 handlers
+
+app.use((req, res) => {
+    res.status(404).send('404 error! url does not exist');
+});
+
+app.use((err, req, res, next) => {
+    if (res.headerSent) {
+        return next(err);
+    }
+
+    return res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => {
+    console.log('Server running on ports'.warn.italic, port);
+});
