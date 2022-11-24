5cc34463ccd6b405639ed997d00f37da90d94cec
aminulislam01101 <aminul.islam01101@gmail.com>
2022-11-24 13:00:05 +0600

    seller collection added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import { MongoClient } from 'mongodb';
 
 // port and env
@@ -20,7 +21,6 @@ app.use((req, res, next) => {
 // Set up default mongoose connection
 const mongoDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dtbllhc.mongodb.net/?retryWrites=true&w=majority`;
 
-
 const client = new MongoClient(mongoDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -28,9 +28,36 @@ const client = new MongoClient(mongoDB, {
 
 const run = async () => {
     try {
-        const productCollection = client.db('books').collection('books');
+        const productCollection = client.db('secondBuy').collection('books');
+        const buyersCollection = client.db('secondBuy').collection('buyers');
+        const sellersCollection = client.db('secondBuy').collection('sellers');
+
         app.post('/books', async (req, res) => {
             res.send(await productCollection.insertOne(req.body));
+        });
+
+        // saving buyer in USERS collection
+
+        app.put('/user/:email', async (req, res) => {
+            const { email } = req.params;
+            const user = req.body;
+            const filter = { email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user,
+            };
+            let result;
+            if (user.role === 'buyer') {
+                result = buyersCollection.updateOne(filter, updatedDoc, options);
+            }
+            if (user.role === 'seller') {
+                result = sellersCollection.updateOne(filter, updatedDoc, options);
+            }
+
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '10d',
+            });
+            res.send({ result, token });
         });
     } finally {
     }
